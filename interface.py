c168f9a3673aa20e32535fc1a31309f988d7948a
Deniz Yürekdeler <>
2019-10-23 13:53:00 +0300

    matplat implementation
    matplotlib interface added
     opencv to matplotconversion learned

--- interface.py
+++ interface.py
@@ -1,9 +1,12 @@
-from skimage import data,io
-from skimage.viewer import ImageViewer
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import matplotlib.image as im
 
-#load img
-img = io.imread('example.png')
+# img = cv2.imread('example.png , 0)
+# plt.imshow( cv2.cvtColor( img, cv2.COLOR_BGR2RGB))  converts opencv read img to plot
 
-#display
-viewer = ImageViewer(img)
-viewer.show()
+img = im.imread('example.png', 0) #read img as original with matplotlib
+plt.imshow(img)
+plt.axis('off')  # hides x,y axises
+plt.show()
