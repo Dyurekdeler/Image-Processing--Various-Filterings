7cf28243544aac80b0de3bc9731ef7bafe6eac62
Dyurekdeler <43982798+Dyurekdeler@users.noreply.github.com>
2019-10-28 23:08:45 +0300

    tkinter and matplotlib
    code allows matplotlib read and shown img inside t kinter

--- GUI.py
+++ GUI.py
@@ -1,27 +1,32 @@
-from tkinter import *
-from tkinter import filedialog
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from tkinter import Frame, Tk
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from numpy import array, arange, sin, pi
 
-class MainMenu():
-    def __init__(self):
-        self.window = Tk()
-        self.window.title("Welcome to LikeGeeks app")
-        self.window.geometry('350x200')
+root = Tk()
+root_panel = Frame(root)
+root_panel.pack(side="bottom", fill="both", expand="yes")
 
-        self.closebutton = Button(self.window, text="Exit", command=self.close)
-        self.closebutton.pack(side='bottom')
-        browsebutton = Button(self.window, text="Browse", command=self.browse)
-        self.browsebutton.pack()
-        self.window.mainloop()
-    def close(self):
-        self.window.destroy()
-    def browse(self):
-        self.filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetype=
-        (("jpeg files", "*.jpg"), ("all files", "*.*")))
-    def matplotCanvas(self):
-        f = Figure(figsize=())
+btn_panel = Frame(root_panel, height=35)
+btn_panel.pack(side='top', fill="both", expand="yes")
 
-MainMenu()
+img_arr = mpimg.imread('winnie.png')
+imgplot = plt.imshow(img_arr)
+
+#here is the example of how I embed matplotlib graph in Tkinter,
+#basically, I want to do the same with the image (imgplot)
+f = Figure()
+a = f.add_subplot(111)
+t = arange(0.0, 3.0, 0.01)
+s = sin(2*pi*t)
+a.imshow(img_arr)
+
+canvas = FigureCanvasTkAgg(f, master=root)
+canvas.draw()
+canvas.get_tk_widget().pack(side="top", fill="both", expand=1)
+canvas._tkcanvas.pack(side="top", fill="both", expand=1)
+
+root.mainloop()
