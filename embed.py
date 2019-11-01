56883b0905f13ac66c50204a67fbcd2ef101e69a
Dyurekdeler <43982798+Dyurekdeler@users.noreply.github.com>
2019-11-01 22:07:23 +0300

    changed gui and added some filters

--- embed.py
+++ embed.py
@@ -1,6 +1,10 @@
 import tkinter
 from tkinter import filedialog
 
+import cv2
+from scipy import ndimage
+from scipy.ndimage import generic_gradient_magnitude
+from skimage.color import rgb2gray
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 # Implement the default Matplotlib key bindings.
@@ -10,50 +14,136 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 import numpy as np
+from skimage.filters import unsharp_mask, sobel
+
 
 class GUI():
 
     def __init__(self):
         self.root = tkinter.Tk()
         self.root.wm_title("Densy's Image World")
+        self.activeCanvas = None
+        self.activeFilename = None
+        self.createWidgets()
+        tkinter.mainloop()
 
-        self.canvas = FigureCanvasTkAgg(self.load(), master=self.root)  # A tk.DrawingArea.
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+    def createWidgets(self):
 
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self.root)
-        self.toolbar.update()
-        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-        self.canvas.mpl_connect("key_press_event", self.on_key_press)
+        fig = Figure(figsize=(5, 4), dpi=100)
+        a = fig.add_subplot(111)
+        canvas = FigureCanvasTkAgg(fig, master=self.root)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
         quitbtn = tkinter.Button(master=self.root, text="Quit", command=self._quit)
         quitbtn.pack(side=tkinter.BOTTOM)
 
-        browsebtn = tkinter.Button(self.root, text="Browse A File", command=self.browse)
+        browsebtn = tkinter.Button(self.root, text="Browse A File", command= lambda: self.browse(a, canvas, 'default'))
         browsebtn.pack()
 
-        tkinter.mainloop()
+        graybtn = tkinter.Button(self.root, text="Convert Gray", command= lambda: self.load(self.activeFilename, a, canvas, 'gray'))
+        graybtn.pack()
 
-    def load(self, filename):
-        imarray = mpimg.imread('winnie.png')
-        plt.imshow(imarray)
-        fig = Figure(figsize=(5, 4), dpi=100)
-        t = np.arange(0, 3, .01)
-        # fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
-        a = fig.add_subplot(111)
-        a.imshow(imarray)
-        return fig
-    #lalala
+        veryblurrybtn = tkinter.Button(self.root, text="Blur A Lot",
+                                 command=lambda: self.load(self.activeFilename, a, canvas, 'very_blurry'))
+        veryblurrybtn.pack()
 
-    def browse(self):
+        sharpbtn = tkinter.Button(self.root, text="Sharpen",
+                                 command=lambda: self.load(self.activeFilename, a, canvas, 'sharp'))
+        sharpbtn.pack()
+
+        motionblurbtn = tkinter.Button(self.root, text="Motion Blur",
+                                 command=lambda: self.load(self.activeFilename, a, canvas, 'motion_blur'))
+        motionblurbtn.pack()
+
+        contrastbtn = tkinter.Button(self.root, text="Contrast",
+                                       command=lambda: self.load(self.activeFilename, a, canvas, 'contrast'))
+        contrastbtn.pack()
+
+        contourbtn = tkinter.Button(self.root, text="Contour Lines",
+                                       command=lambda: self.load(self.activeFilename, a, canvas, 'contour'))
+        contourbtn.pack()
+        edgebtn = tkinter.Button(self.root, text="Find Edges",
+                                       command=lambda: self.load(self.activeFilename, a, canvas, 'find_edges'))
+        edgebtn.pack()
+        cropbtn = tkinter.Button(self.root, text="Crop",
+                                 command=lambda: self.load(self.activeFilename, a, canvas, 'crop'))
+        cropbtn.pack()
+        if(self.activeCanvas is None):
+            toolbar = NavigationToolbar2Tk(canvas, self.root)
+            toolbar.update()
+            canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    def browse(self,a,canvas,mode):
         filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetype=
         (("jpeg files", "*.jpg"), ("all files", "*.*")))
-        self.load(filename)
+        self.activeFilename = filename
+        self.load(filename,a,canvas,mode)
 
-    def on_key_press(self, event):
-        print("you pressed {}".format(event.key))
-        key_press_handler(event, self.canvas, self.toolbar)
+    def load(self, filename,a,canvas,mode):
+        imarray = mpimg.imread(filename)
+        if mode == 'default':
+            plt.imshow(imarray)
+            t = np.arange(0, 3, .01)
+            a.imshow(imarray)
+
+        elif mode == 'gray':
+            gray = rgb2gray(imarray)
+            plt.imshow(imarray)
+            t = np.arange(0, 3, .01)
+            a.imshow(gray, cmap=plt.cm.gray)
+
+        elif mode == 'very_blurry':
+            very_blurred = ndimage.uniform_filter(imarray, size=(11, 11, 1))
+            plt.imshow(imarray)
+            t = np.arange(0, 3, .01)
+            a.imshow(very_blurred)
+
+        elif mode == 'sharp':
+            sharp = unsharp_mask(imarray, radius=5, amount=2)
+            plt.imshow(imarray)
+            t = np.arange(0, 3, .01)
+            a.imshow(sharp)
+
+        elif mode == 'motion_blur':
+            size = 50
+            # generating the kernel
+            kernel_motion_blur = np.zeros((size, size))
+            kernel_motion_blur[int((size - 1) / 2), :] = np.ones(size)
+            kernel_motion_blur = kernel_motion_blur / size
+
+            # applying the kernel to the input image
+            motionblur = cv2.filter2D(imarray, -1, kernel_motion_blur)
+            plt.imshow(imarray)
+            t = np.arange(0, 3, .01)
+            a.imshow(motionblur)
+
+        elif mode == 'find_edges':
+            edgey = generic_gradient_magnitude(imarray, sobel)
+            plt.imshow(imarray)
+            t = np.arange(0, 3, .01)
+            a.imshow(edgey)
+
+        elif mode == 'contrast':
+            plt.imshow(imarray, vmin=30, vmax=200)
+            t = np.arange(0, 3, .01)
+            a.imshow(imarray)
+
+        elif mode == 'contour':
+            plt.imshow(imarray)
+            plt.contour(imarray, [50, 200])
+            t = np.arange(0, 3, .01)
+            a.imshow(imarray)
+        #HISTOGRAM
+        elif mode == 'crop':
+            crop = imarray[77:141, 57:121]
+            plt.imshow(imarray)
+            plt.contour(imarray, [50, 200])
+            t = np.arange(0, 3, .01)
+            a.imshow(crop)
+        canvas.draw()
+
+
 
     def _quit(self):
         self.root.quit()     # stops mainloop
