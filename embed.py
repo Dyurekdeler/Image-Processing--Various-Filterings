ebcc31ddcb0b372bfebc12289b262c5bc3692f67
denizyu <dyurekdeler@live.com>
2019-11-09 15:54:46 +0300

    added 1 normal filtering and equalization hist plot fixed, closes iwndows at the start of each filter and added intensituy

--- embed.py
+++ embed.py
@@ -49,26 +49,26 @@ class GUI():
         browsebtn = tkinter.Button(self.root, text="Browse A File", command= lambda: self.browse(a, canvas, 'default'))
         browsebtn.pack()
 
+        label = tkinter.Label(self.root, text="Enter your weight in pounds.")
+        label.pack()
+
+        self.weight_in_kg = tkinter.StringVar()
+        tkinter.Entry(self.root, textvariable=self.weight_in_kg).pack()
+
         combo = ttk.Combobox(self.root,
                                     values=[
-                                        "gray",
-                                        "very_blurry",
-                                        "sharp",
-                                        "motion_blur",
-                                        "contrast","contour","find_edges","lighten","darken","invert","gaussian",
-                                        "percentile_mean","bilateral_mean",
+                                        "gray","very_blurry","sharp","mean",
+                                        "motion_blur","lighten","darken","invert","gaussian",
+                                        "percentile_mean","bilateral_mean",'find_edges',
 
-                                    "crop","rescale","resize","downscaled","rotate","swirl",
+                                        "crop","rescale","resize","downscaled","rotate","swirl",
                                     
-                                    "intensity",
+                                        "intensity",
 
-                                    "mean",
+                                        "histogram","equalization",
 
-                                    "histogram","equalization",
-
-                                    "erosion","dilation","opening","closing","morp_gradient","top_hat","black_hat"])
+                                        "erosion","dilation","opening","closing","morp_gradient","top_hat","black_hat"])
         combo.current(0)
-
         combo.bind("<<ComboboxSelected>>", self.callbackFunc)
         combo.pack()
         self.combobox = combo
@@ -83,7 +83,7 @@ class GUI():
             canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
     def callbackFunc(self, event):
-        print("New Element is = "+ self.combobox.get())
+        print("New Element is = "+ self.combobox.get()+" : "+ self.weight_in_kg.get())
 
     def browse(self,a,canvas,mode):
         filename = filedialog.askopenfilename(initialdir="/home/denizyu/Pictures", title="Select A File", filetypes=
@@ -91,29 +91,23 @@ class GUI():
         self.activeFilename = filename
         self.load(filename,a,canvas,mode)
 
-
     def load(self, filename,a,canvas,mode):
         imarray = mpimg.imread(filename)
+        plt.close('all')
         if mode == 'default':
-
             a.imshow(imarray)
 
+        #IMG FILTERING
         elif mode == 'gray':
             gray = rgb2gray(imarray)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(gray, cmap=plt.cm.gray)
 
         elif mode == 'very_blurry':
             very_blurred = ndimage.uniform_filter(imarray, size=(11, 11, 1))
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(very_blurred)
 
         elif mode == 'sharp':
             sharp = unsharp_mask(imarray, radius=5, amount=2)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(sharp)
 
         elif mode == 'motion_blur':
@@ -125,25 +119,7 @@ class GUI():
 
             # applying the kernel to the input image
             motionblur = cv2.filter2D(imarray, -1, kernel_motion_blur)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(motionblur)
-
-        elif mode == 'find_edges':
-            """does not work with 3d"""
-            gray = rgb2gray(imarray)
-            edgey = generic_gradient_magnitude(gray, sobel)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
-            a.imshow(edgey)
-
-        elif mode == 'contrast':
-            """does not work with 3d
-            gray = rgb2gray(imarray)
-            plt.imshow(gray, vmin=30, vmax=200)
-            t = np.arange(0, 3, .01)
-            a.imshow(gray, cmap="gray")"""
-            # -----Converting image to LAB Color model-----------------------------------
 
         elif mode == 'mean':
             image = cv2.cvtColor(imarray, cv2.COLOR_BGR2HSV)  # convert to HSV
@@ -154,100 +130,80 @@ class GUI():
             plt.xticks([]), plt.yticks([])
             plt.subplot(122), plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_HSV2RGB)), plt.title('Mean filter')
             plt.xticks([]), plt.yticks([])
-            plt.imshow(imarray)
             a.imshow(new_image)
 
-        elif mode == 'contour':
+        elif mode == 'find_edges':
             gray = rgb2gray(imarray)
-            plt.imshow(imarray)
-            plt.contour(gray, [50, 200])
-            t = np.arange(0, 3, .01)
-            a.imshow(gray, cmap="gray")
+            edge = sobel(gray)
+            a.imshow(edge)
 
         elif mode == "lighten":
             image = cv2.add(imarray, np.array([50.0]))
-            plt.imshow(image)
             a.imshow(image)
 
         elif mode == "darken":
             image = cv2.subtract(imarray, np.array([50.0]))
-            plt.imshow(image)
             a.imshow(image)
 
         elif mode == "invert":
             image = np.invert(imarray)
-            plt.imshow(image)
             a.imshow(image)
 
         elif mode == "gaussian":
             image = gaussian_filter(imarray, sigma=1)
-            plt.imshow(image)
             a.imshow(image)
 
         elif mode == "percentile_mean":
             gray = rgb2gray(imarray)
             selem = disk(20)
             percentile_result = rank.mean_percentile(gray, selem=selem, p0=.1, p1=.9)
-            plt.imshow(percentile_result)
             a.imshow(percentile_result, cmap="gray")
 
         elif mode == "bilateral_mean":
             gray = rgb2gray(imarray)
             selem = disk(20)
             bilateral_result = rank.mean_bilateral(gray, selem=selem, s0=500, s1=500)
-            plt.imshow(bilateral_result)
             a.imshow(bilateral_result, cmap="gray")
 
-        #uzaysal
+        #SPATIAL
         elif mode == 'crop':
             """does not work with 3d"""
             gray = rgb2gray(imarray)
             crop = gray[30:141, 15:140]
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(crop, cmap="gray")
 
         elif mode == 'rescale':
             gray= rgb2gray(imarray)
             image_rescaled = rescale(gray, 0.25, anti_aliasing=False)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(image_rescaled, cmap='gray')
 
         elif mode == 'resize':
             gray= rgb2gray(imarray)
             image_resized = resize(gray, (gray.shape[0] // 4, gray.shape[1] // 4),
                                    anti_aliasing=True)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(image_resized, cmap='gray')
 
         elif mode == 'downscaled':
             gray= rgb2gray(imarray)
             image_downscaled = downscale_local_mean(gray, (4, 3))
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(image_downscaled, cmap='gray')
 
         elif mode == 'rotate':
             new_pic = rotate(imarray, 180)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(new_pic)
 
         elif mode == 'swirl':
             swirled = swirl(imarray, rotation=0, strength=10, radius=250)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
             a.imshow(swirled)
 
-        #intensity adjustment
+        #INTENSITY ADJUSTMENT
         elif mode == 'intensity':
-            new_image = cv2.convertScaleAbs(imarray, alpha=1, beta=2.54)
-            plt.imshow(imarray)
-            t = np.arange(0, 3, .01)
-            a.imshow(new_image)
+                gamma = self.weight_in_kg.get()
+                # Apply gamma correction.
+                gamma_corrected = np.array(255 * (imarray / 255) ** gamma, dtype='uint8')
+                a.imshow(gamma_corrected)
 
+                #HISTOGRAM AND EQUALIZATION
         elif mode == 'histogram':
             img = cv2.imread(filename, 0)
             gray = rgb2gray(img)
@@ -259,59 +215,43 @@ class GUI():
             gray = rgb2gray(img)
             equ = cv2.equalizeHist(gray)
             a.imshow(equ, cmap='gray')
-
-        elif mode == "opencv":
-            img = cv2.imread(filename)
-            color = ('b', 'g', 'r')
-            for i, col in enumerate(color):
-                histr = cv2.calcHist([img], [i], None, [256], [0, 256])
-                plt.plot(histr, color=col)
-                plt.xlim([0, 256])
+            plt.hist(equ.ravel(), 256, [0, 256])
             plt.show()
 
+        #MORPHOLOGICAL
         elif mode == 'erosion':
             kernel = np.ones((5, 5), np.uint8)
             erosion = cv2.erode(imarray, kernel, iterations=1)
-            plt.imshow(imarray)
-
             a.imshow(erosion)
 
         elif mode == 'dilation':
             kernel = np.ones((5, 5), np.uint8)
             dilation = cv2.dilate(imarray,kernel,iterations = 1)
-            plt.imshow(imarray)
-
             a.imshow(dilation)
 
         elif mode == 'opening':
             kernel = np.ones((5, 5), np.uint8)
             erosion = cv2.erode(imarray, kernel, iterations=1)
-            plt.imshow(imarray)
-
             a.imshow(erosion)
+
         elif mode == 'closing':
             kernel = np.ones((5, 5), np.uint8)
             closing = cv2.morphologyEx(imarray, cv2.MORPH_CLOSE, kernel)
-            plt.imshow(imarray)
-
             a.imshow(closing)
 
         elif mode == 'morp_gradient':
             kernel = np.ones((5, 5), np.uint8)
             gradient = cv2.morphologyEx(imarray, cv2.MORPH_GRADIENT, kernel)
-            plt.imshow(imarray)
             a.imshow(gradient)
 
         elif mode == 'top_hat':
             kernel = np.ones((5, 5), np.uint8)
             tophat = cv2.morphologyEx(imarray, cv2.MORPH_TOPHAT, kernel)
-            plt.imshow(imarray)
             a.imshow(tophat)
 
         elif mode == 'black_hat':
             kernel = np.ones((5, 5), np.uint8)
             blackhat = cv2.morphologyEx(imarray, cv2.MORPH_BLACKHAT, kernel)
-            plt.imshow(imarray)
             a.imshow(blackhat)
 
         canvas.draw()
